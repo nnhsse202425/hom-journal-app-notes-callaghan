9947e26dd1c8f3cf1b6e26f5fdc1fe6e23c4c4c6
Matt Callaghan <mcallaghan@naperville203.org>
2024-12-04 11:02:31 -0600

    Add createEntry route and page.

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * contains client-side Javascript functions
+ *  (primarily event handlers to fetch data from the Node server)
+ */
+
+const createEntryButton = document.querySelector("button.createEntry");
+
+createEntryButton.addEventListener("click", () => {
+  // redirect the client (browser) to the /createEntry path
+  window.location = "/createEntry";
+});
